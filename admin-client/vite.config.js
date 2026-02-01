43e52baa8d0c931cf3d5a658622b14806940aea5
KUiMEi1126 <1581825854@qq.com>
2026-02-01 14:54:00 +0800

    项目仓库初始化

---
+++ admin-client/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5174, // 移动端用 5173，PC端可以改成 5174 防止冲突
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // 后端地址
+        changeOrigin: true,
+      }
+    }
+  }
+})
