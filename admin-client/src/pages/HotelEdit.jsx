c6da6df8939d5aea75d483e5bdb61eb63e8dbae6
KUiMEi1126 <1581825854@qq.com>
2026-02-10 17:16:23 +0800

    根据数据结构重新设定界面

--- admin-client/src/pages/HotelEdit.jsx
+++ admin-client/src/pages/HotelEdit.jsx
@@ -1,52 +1,119 @@
 import React from 'react';
-import { Form, Input, InputNumber, Button, Select, Upload, message } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import locale from 'antd/locale/zh_CN';
+import { Form, Input, Button, DatePicker, Rate, Space, Card, message } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
 const HotelEdit = () => {
   const [form] = Form.useForm();
 
   const onFinish = (values) => {
-    console.log('提交的数据:', values);
-    // 这里将来写: axios.post('/api/hotels', values)
-    message.success('保存成功！');
+    // values.openedAt 是 moment 对象，需要转成字符串
+    const submitData = {
+      ...values,
+      openedAt: values.openedAt ? values.openedAt.format('YYYY-MM-DD') : '',
+    };
+    console.log('提交给后端的数据:', submitData);
+    message.success('保存成功！请看控制台数据结构');
   };
 
   return (
-    <div style={{ maxWidth: 600 }}>
-      <h2>录入/编辑酒店信息</h2>
-      <Form
-        form={form}
-        layout="vertical"
-        onFinish={onFinish}
-      >
-        <Form.Item label="酒店名称" name="name" rules={[{ required: true, message: '请输入名称' }]}>
-          <Input placeholder="例如：北京万豪酒店" />
-        </Form.Item>
-
-        <Form.Item label="酒店地址" name="address" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="基础价格" name="price" rules={[{ required: true }]}>
-          <InputNumber prefix="￥" style={{ width: '100%' }} />
-        </Form.Item>
-
-        <Form.Item label="星级" name="star">
-           <Select>
-              <Select.Option value={3}>三星级</Select.Option>
-              <Select.Option value={4}>四星级</Select.Option>
-              <Select.Option value={5}>五星级</Select.Option>
-           </Select>
-        </Form.Item>
+    <div style={{ padding: 24, background: '#fff' }}>
+      <h2>酒店信息录入</h2>
+      <Form form={form} layout="vertical" onFinish={onFinish} initialValues={{ star: 3 }}>
         
-        <Form.Item label="酒店图片">
-            <Upload listType="picture-card">
-                <div><PlusOutlined /><div style={{ marginTop: 8 }}>上传</div></div>
-            </Upload>
-        </Form.Item>
+        {/* 1. 基础信息区 */}
+        <Card title="基础信息" bordered={false}>
+          <Space size="large" style={{ display: 'flex' }}>
+            <Form.Item label="酒店中文名" 
+              name="nameCn" 
+              rules={[
+                { 
+                  required: true, 
+                  message: '该项为必填项'
+                }
+              ]}
+            >
+              <Input placeholder="例如：希尔顿酒店" style={{ width: 300 }} />
+            </Form.Item>
+            <Form.Item label="酒店英文名" 
+              name="nameEn" 
+              rules={[
+                { 
+                  required: true, 
+                  message: '该项为必填项'
+                }
+              ]}
+            >
+              <Input placeholder="E.g. Hilton Hotel" style={{ width: 300 }} />
+            </Form.Item>
+          </Space>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">保存并发布</Button>
+          <Form.Item label="详细地址" 
+            name="address" 
+            rules={[
+                { 
+                  required: true, 
+                  message: '该项为必填项'
+                }
+              ]}
+            >
+            <Input placeholder="例如：北京市朝阳区xx街道xx号" />
+          </Form.Item>
+
+          <Space size="large">
+            <Form.Item label="星级" name="star">
+              <Rate />
+            </Form.Item>
+            <Form.Item label="开业时间" 
+              name="openedAt" 
+              rules={[
+                  { 
+                    required: true, 
+                    message: '该项为必填项'
+                  }
+               ]}
+              >
+              <DatePicker placeholder="请选择日期" />
+            </Form.Item>
+          </Space>
+        </Card>
+
+        {/* 2. 房型管理区 (动态表单 ) */}
+        <Card title="房型与价格管理" bordered={false} style={{ marginTop: 20 }}>
+          <Form.List name="rooms">
+            {(fields, { add, remove }) => (
+              <>
+                {fields.map(({ key, name, ...restField }) => (
+                  <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'name']}
+                      rules={[{ required: true, message: '输入房型' }]}
+                    >
+                      <Input placeholder="房型名称 (如: 标准间)" />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'price']}
+                      rules={[{ required: true, message: '输入价格' }]}
+                    >
+                      <Input prefix="￥" placeholder="价格" type="number" />
+                    </Form.Item>
+                    <MinusCircleOutlined onClick={() => remove(name)} />
+                  </Space>
+                ))}
+                <Form.Item>
+                  <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                    添加一种房型
+                  </Button>
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
+        </Card>
+
+        <Form.Item style={{ marginTop: 20 }}>
+          <Button type="primary" htmlType="submit" size="large">保存并发布</Button>
         </Form.Item>
       </Form>
     </div>
